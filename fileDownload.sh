1e4fb6b6dca93cd6f2cdd0e7d7d3050957205a96
deepsmittal <50650219+deepsmittal@users.noreply.github.com>
2022-02-23 16:24:45 +0530

    Update fileDownload.sh

--- fileDownload.sh
+++ fileDownload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -o /Users/chef/sample-wav.wav https://file-examples-com.github.io/uploads/2017/11/file_example_WAV_1MG.wav
+curl -o /Users/chef/Downloads/sample-wav.wav https://file-examples-com.github.io/uploads/2017/11/file_example_WAV_1MG.wav
 curl -o /Users/chef/sample.pdf http://www.africau.edu/images/default/sample.pdf
